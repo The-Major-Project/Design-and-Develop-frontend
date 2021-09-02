aa82b38a4ad14481efcc2cade1fbe2582e2dc860
Mihir Verma <mihirv7781@gmail.com>
2021-09-02 23:58:01 +0530

    initial release

---
+++ src/modules/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import logo from '../assets/logo512.png'
+const Home = () => {
+    return (
+        <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+        <div class="flex-shrink-0">
+          <img class="h-12 w-12" src={logo} alt="ChitChat Logo"/>
+        </div>
+        <div>
+          <div class="text-xl font-medium text-black">React Boilerplate</div>
+          <p class="text-gray-500">Hello from mihir verma</p>
+        </div>
+      </div>
+    )
+}
+
+export default Home
