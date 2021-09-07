097331b1c84c4920b1f67e6e044ca387d1f0cd78
Mihir Verma <mihirv7781@gmail.com>
2021-09-07 22:27:01 +0530

    added custom colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,36 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      black: "2d2d2d",
+      white: "ffffff",
+      blue: {
+        hero: "#CBE7FD",
+        primary: "#005CE6",
+        light: "#EFF5FF",
+        border: "#BDD8FF",
+      },
+      grey: {
+        light: "#9E9E9E",
+        dark: "#5B5B5B",
+      },
+      pink: {
+        light: "#FEE7F3",
+        dark: "#FF014E",
+      },
+      gold: {
+        light: "#FFF7E4",
+        dark: "#FFB400",
+      },
+      shadow: {
+        light: "#D8E7FF",
+        dark: "#7CB0FF",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
