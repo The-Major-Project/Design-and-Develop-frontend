cb6a4e273f1ebf1ca4e23fb6f65c8634047c9240
Yash Sharma <yashsharma1859@gmail.com>
2021-09-16 17:46:47 +0530

    Improved the code of loader component

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,39 +1,26 @@
 import React from "react";
-import cx from "classnames";
 
-const Loader = ({ bg = "#fff", color = "fff" }) => {
-	let classnames = cx(
-		{
-			// "h-3 w-3": size === "xs",
-			// "h-4 w-4": size === "sm",
-			// "h-5 w-5": size === "md",
-			// "h-6 w-6": size === "lg",
-			// "h-8 w-8": size === "xl",
-		},
-		`animate-spin -ml-1 mr-3 h-4 w-4  text-white`
-	);
-	return (
-		<svg
-			className={classnames}
-			xmlns="http://www.w3.org/2000/svg"
-			fill="none"
-			viewBox="0 0 24 24"
-		>
-			<circle
-				className="opacity-25"
-				cx="12"
-				cy="12"
-				r="10"
-				stroke={bg}
-				strokeWidth="4"
-			></circle>
-			<path
-				className="opacity-75"
-				fill={color}
-				d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-			></path>
-		</svg>
-	);
-};
+const Loader = ({ bg = "#fff", color = "fff" }) => (
+	<svg
+		className="animate-spin -ml-1 mr-3 h-4 w-4  text-white"
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+	>
+		<circle
+			className="opacity-25"
+			cx="12"
+			cy="12"
+			r="10"
+			stroke={bg}
+			strokeWidth="4"
+		></circle>
+		<path
+			className="opacity-75"
+			fill={color}
+			d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+		></path>
+	</svg>
+);
 
 export default Loader;
