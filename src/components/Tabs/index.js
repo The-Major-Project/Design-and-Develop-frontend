2cd2ac606905fc6aafca86d622f3c7823761d898
Mihir Verma <mihirv7781@gmail.com>
2021-10-02 14:52:46 +0530

    Implemented Tab Component

---
+++ src/components/Tabs/index.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+const Tabs = ({ data }) => {
+  const [openTab, setOpenTab] = React.useState(1);
+
+  return (
+    <>
+      <div>
+        <ul
+          className="flex list-none overflow-x-scroll mb-6 no-scrollbar md:justify-center "
+          role="tablist"
+        >
+          {data.map(({ id, tabname }) => {
+            return (
+              <li className="mr-4 text-center mt-4 flex justify-center ">
+                <a
+                  className={
+                    "text-xs font-bold uppercase px-8 py-2  rounded-full  " +
+                    (openTab === id
+                      ? "text-white bg-" + "blue" + "-600"
+                      : "text-gray-400 bg-white ")
+                  }
+                  onClick={(e) => {
+                    e.preventDefault();
+                    setOpenTab(id);
+                  }}
+                  data-toggle="tab"
+                  href={`link${id}`}
+                  role="tablist"
+                >
+                  {tabname}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+
+        <div className="tab-content tab-space flex w-full justify-between ">
+          {data.map(({ id, tabdata }) => {
+            return (
+              <div
+                className={openTab === id ? "block" : "hidden"}
+                id={`link${id}`}
+              >
+                <div className="w-full flex flex-wrap justify-center gap-6">
+                  {tabdata}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Tabs;
--- src/modules/dashboard/Happners/index.js
+++ src/modules/dashboard/Happners/index.js
@@ -1,13 +1,60 @@
 import React from "react";
 import Layout from "../../../components/Layout";
 import SideMenu from "../../../components/SideMenu";
+import Tabs from "../../../components/Tabs";
+import HappnersCard from "../../../components/HappnersCard";
 
 const Happners = () => {
-	return (
-		<>
-			<Layout sider={<SideMenu />} main={<div></div>} />
-		</>
-	);
+  const data = [
+    {
+      id: 1,
+      tabname: "Suggestions",
+      tabdata: (
+        <>
+          <HappnersCard name="Mihir Verma" />
+          <HappnersCard name="Yash Sharma" />
+          <HappnersCard name="Manvi Jain" />
+          <HappnersCard name="Meemansha Jain" />
+        </>
+      ),
+    },
+    {
+      id: 2,
+      tabname: "Followers",
+      tabdata: (
+        <>
+          <HappnersCard name="Yash Sharma" />
+          <HappnersCard name="Manvi Jain" />
+          <HappnersCard name="Mihir Verma" />
+          <HappnersCard name="Meemansha Jain" />
+        </>
+      ),
+    },
+    {
+      id: 3,
+      tabname: "Following",
+      tabdata: (
+        <>
+          <HappnersCard name="Meemansha Jain" />
+          <HappnersCard name="Yash Sharma" />
+          <HappnersCard name="Mihir Verma" />
+          <HappnersCard name="Manvi Jain" />
+        </>
+      ),
+    },
+  ];
+  return (
+    <>
+      <Layout
+        sider={<SideMenu />}
+        main={
+          <div>
+            <Tabs data={data} />
+          </div>
+        }
+      />
+    </>
+  );
 };
 
 export default Happners;
