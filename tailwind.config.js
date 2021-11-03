19aed9f75c1307aa3b941565bbec763f34544790
Mihir Verma <mihirv7781@gmail.com>
2021-11-03 15:19:45 +0530

    changes to tabs component

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,15 @@ module.exports = {
 		extend: {
 			boxShadow: {
 				btnShadow: "15px 15px 37px 0 rgba(0, 92, 230, 0.35)",
-				tabShadow: "5px 10px 20px 0 rgba(0, 92, 230, 0.15)",
+				tabShadow: "0px 0px 20px 0 rgba(0, 92, 230, 0.35)",
+				tabShadows: "0px 0px 20px 0 rgba(0, 92, 230, 0.14)",
 				siderShadow: "15px 15px 15px 0 rgba(0, 92, 230, 0.04)",
 			},
 			spacing: {
 				85: "23rem",
 				68: "16.5rem",
 				100: "30rem",
+				110: "35rem",
 			},
 			backgroundColor: {
 				modalBG: "rgba(0, 0, 0, 0.4)",
