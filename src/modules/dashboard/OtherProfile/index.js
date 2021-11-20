658620c390b502d03fa831fae16842f7f0f7c42a
Mihir Verma <mihirv7781@gmail.com>
2021-11-20 22:24:05 +0530

    fixed otherprofile bug

--- src/modules/dashboard/OtherProfile/index.js
+++ src/modules/dashboard/OtherProfile/index.js
@@ -16,185 +16,183 @@ import axios from "axios";
 import OtherProfileOverview from "./OtherProfileOverview";
 
 const OtherProfile = () => {
-	const params = useParams();
-	const [gitRepos, setGitRepos] = useState([]);
-	// eslint-disable-next-line no-unused-vars
-	const [urlUser, setUrlUser] = useState({
-		name: "",
-		usertype: "",
-		address: "",
-		email: "",
-		portfolioURL: "",
-		dribbbleusername: "",
-		githubUsername: "",
-		followers: "",
-		following: "",
-	});
-	const [userPost, setUserPost] = useState([]);
-	// const [self, setSelf] = useState();
-	// const [otherUser, setOtherUser] = useState({});
+  const params = useParams();
+  const [gitRepos, setGitRepos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  // eslint-disable-next-line no-unused-vars
+  const [urlUser, setUrlUser] = useState({
+    name: "",
+    usertype: "",
+    address: "",
+    email: "",
+    portfolioURL: "",
+    dribbbleusername: "",
+    githubUsername: "",
+    followers: "",
+    following: "",
+  });
+  const [userPost, setUserPost] = useState([]);
 
-	useEffect(() => {
-		const getOtherUSer = async () => {
-			try {
-				const res = await axiosInstance.get(`/api/user/${params.id}`, {
-					headers: {
-						Accept: "application/json",
-						"Content-Type": "application/json",
-						"Access-Control-Allow-Origin": "*",
-						accessToken: localStorage.getItem("accessToken"),
-					},
-				});
-				const gitPost = await axios.get(
-					`https://api.github.com/users/${res.data.data.githubusername}/repos`
-				);
-				setUrlUser(res);
-				setGitRepos(gitPost.data);
-				console.log("Thisssdsdsdsdsdsddsd", urlUser);
-				console.log(gitRepos);
-			} catch (err) {
-				console.log(err);
-			}
-		};
-		getOtherUSer();
-	}, []);
+  useEffect(() => {
+    const getOtherUSer = async () => {
+      try {
+        const res = await axiosInstance.get(`/api/user/${params.id}`, {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            accessToken: localStorage.getItem("accessToken"),
+          },
+        });
+        const gitPost = await axios.get(
+          `https://api.github.com/users/${res.data.data.githubusername}/repos`
+        );
+        setUrlUser(res.data.data);
+        setGitRepos(gitPost.data);
+		setLoading(false)
+        console.log("Thisssdsdsdsdsdsddsd", urlUser);
+        console.log(gitRepos);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getOtherUSer();
+  }, []);
 
-	useEffect(() => {
-		const getUser = async () => {
-			try {
-				const resPost = await axiosInstance.get(`/api/posts/${params.id}/all`, {
-					headers: {
-						Accept: "application/json",
-						"Content-Type": "application/json",
-						"Access-Control-Allow-Origin": "*",
-						accessToken: localStorage.getItem("accessToken"),
-						userId: params.id,
-					},
-				});
-				setUserPost(resPost.data.data);
-				// const dribShots = await axios.get(
-				//   `https://api.dribbble.com/v2/user/shots?access_token=`
-				// );
-				// console.log(dribShots);
-			} catch (error) {
-				console.log(error);
-			}
-		};
-		getUser();
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const resPost = await axiosInstance.get(`/api/posts/${params.id}/all`, {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            accessToken: localStorage.getItem("accessToken"),
+            userId: params.id,
+          },
+        });
+        setUserPost(resPost.data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getUser();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-	// useEffect(() => {
-	// 	console.log("this is param id = ", params.id);
-	// 	if (params.id === localStorage.getItem("userId")) {
-	// 		setSelf(true);
-	// 	} else {
-	// 		setSelf(false);
-	// 	}
-	// }, [params.id]);
 
-	const data = [
-		{
-			id: 1,
-			tabname: "Posts",
-			tabdata: (
-				<>
-					{userPost.map((post) => {
-						return (
-							<ProfilePost
-								title={post.title}
-								description={post.description}
-								requiredDesigner={post.designer}
-								developer={post.developer}
-								key={post._id}
-								self={false}
-								date={post.updatedAt.substring(0, 10)}
-							/>
-						);
-					})}
-				</>
-			),
-		},
-		{
-			id: 2,
-			tabname: "Repos",
-			active: <GithubWhite width="18" />,
-			inactive: <GithubBlack width="18" />,
-			tabdata: (
-				<>
-					{gitRepos.map((repo) => {
-						return (
-							<GithubProfileCard
-								title={repo.name}
-								description={repo.description}
-								fork={repo.forks_count}
-								star={repo.stargazers_count}
-								key={repo.id}
-								language={repo.language}
-							/>
-						);
-					})}
-				</>
-			),
-		},
-		{
-			id: 3,
-			tabname: "Shots",
-			active: <DribbbleWhite width="18" />,
-			inactive: <DribbbleBlack width="18" />,
-			tabdata: (
-				<>
-					<DribbbleShotsCard
-						coverImage="https://cdn.dribbble.com/users/5994307/screenshots/16739125/media/c01a043f87a54820d9845eb60fbd94e8.png"
-						title="Testify - Issue and Bug Tracking"
-						like="50"
-						view="26"
-					/>
-					<DribbbleShotsCard
-						coverImage="https://cdn.dribbble.com/users/2498386/screenshots/16730603/media/383419e66576b479df2892019d84f934.png"
-						title="Insurance App"
-						like="40"
-						view="76"
-					/>
-					<DribbbleShotsCard
-						coverImage="https://cdn.dribbble.com/users/2685252/screenshots/16736995/media/d805c7cba191166c80bdca12598cd850.png"
-						title="Ae - NFT Marketplace Website"
-						like="52"
-						view="21"
-					/>
-				</>
-			),
-		},
-	];
-	return (
-		<>
-			<Layout
-				sider={<SideMenu />}
-				main={
-					<div className="flex flex-col lg:flex-row w-full ">
-						{urlUser && (
-							<OtherProfileOverview
-								self={false}
-								name={urlUser.name}
-								usertype={urlUser.usertype}
-								address={urlUser.address}
-								email={urlUser.email}
-								portfolioURL={urlUser.portfolioURL}
-								dribbbleusername={urlUser.dribbbleusername}
-								githubUsername={urlUser.githubUsername}
-								followers={urlUser.followers}
-								following={urlUser.following}
-							/>
-						)}
-
-						<div className="w-full flex lg:pl-10">
-							<Tabs data={data} />
-						</div>
-					</div>
-				}
+const gitData = {
+	id: 2,
+	tabname: "Repos",
+	active: <GithubWhite width="18" />,
+	inactive: <GithubBlack width="18" />,
+	tabdata: (
+	  <>
+		{gitRepos.map((repo) => {
+		  return (
+			<GithubProfileCard
+			  title={repo.name}
+			  description={repo.description}
+			  fork={repo.forks_count}
+			  star={repo.stargazers_count}
+			  key={repo.id}
+			  language={repo.language}
 			/>
-		</>
-	);
+		  );
+		})}
+	  </>
+	),
+  }
+
+  const dribData  = {
+	id: 3,
+	tabname: "Shots",
+	active: <DribbbleWhite width="18" />,
+	inactive: <DribbbleBlack width="18" />,
+	tabdata: (
+	  <>
+		<DribbbleShotsCard
+		  coverImage="https://cdn.dribbble.com/users/5994307/screenshots/16739125/media/c01a043f87a54820d9845eb60fbd94e8.png"
+		  title="Testify - Issue and Bug Tracking"
+		  like="50"
+		  view="26"
+		/>
+		<DribbbleShotsCard
+		  coverImage="https://cdn.dribbble.com/users/2498386/screenshots/16730603/media/383419e66576b479df2892019d84f934.png"
+		  title="Insurance App"
+		  like="40"
+		  view="76"
+		/>
+		<DribbbleShotsCard
+		  coverImage="https://cdn.dribbble.com/users/2685252/screenshots/16736995/media/d805c7cba191166c80bdca12598cd850.png"
+		  title="Ae - NFT Marketplace Website"
+		  like="52"
+		  view="21"
+		/>
+	  </>
+	),
+  }
+
+  const data = [
+    {
+      id: 1,
+      tabname: "Posts",
+      tabdata: (
+        <>
+          {userPost.map((post) => {
+            return (
+              <ProfilePost
+                title={post.title}
+                description={post.description}
+                requiredDesigner={post.designer}
+                developer={post.developer}
+                key={post._id}
+                self={false}
+                date={post.updatedAt.substring(0, 10)}
+              />
+            );
+          })}
+        </>
+      ),
+    },
+	// can be fixed
+urlUser.userType === "Developer" || "both" ?gitData:{},
+urlUser.userType === "Designer" || "both" ?dribData:{}
+    
+  ];
+  return (
+    <>
+      <Layout
+        sider={<SideMenu />}
+        main={
+          <div className="flex flex-col lg:flex-row w-full ">
+            {loading ? (
+              <div className="">loading state</div>
+            ) : (
+              <OtherProfileOverview
+                self={false}
+                name={urlUser.name}
+                usertype={urlUser.usertype}
+                address={urlUser.address}
+                email={urlUser.email}
+                portfolioURL={urlUser.profileurl}
+                profileimage={urlUser.profileimage}
+                dribbbleusername={urlUser.dribbbleusername}
+                githubusername={urlUser.githubusername}
+                followers={urlUser.followers}
+                following={urlUser.following}
+				description={urlUser.description}
+              />
+            )}
+
+            <div className="w-full flex lg:pl-10">
+              <Tabs data={data} />
+            </div>
+          </div>
+        }
+      />
+    </>
+  );
 };
 
 export default OtherProfile;
